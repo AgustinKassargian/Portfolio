8d6a83988a2f5e0d4a51037423b4d6d11f30844c
AgustinKassargian <agustinkassargian@gmail.com>
2023-02-07 12:14:01 -0300

    upload image

--- client/src/components/Projects/projects.ts
+++ client/src/components/Projects/projects.ts
@@ -1,7 +1,7 @@
 export const projects : any = [{
     title: "Foodify App",
     description: "Foodify is a Web Application with one purpose: Improve care and experience of the gastronomic establishment that implements it.It allows getting rid of physical menus, providing an online menu, the possibility of taking orders from a mobile device and charging them. It also allows the menu to be modified and the client to call the waiter from her seat without making eye contact.",
-    img: 'https://res.cloudinary.com/foodify-henry/image/upload/v1674501198/Foodify_lrxw30.png',
+    img: 'https://res.cloudinary.com/foodify-henry/image/upload/v1675782737/Foodify_g9zv6j.png',
     links:{
         direct: "https://foodify-ten.vercel.app/",
         github: "https://github.com/AgustinKassargian/Foodify-App",
